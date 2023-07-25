2215a77b323fcee8dca9abaaeaef62ee50d3a474
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-07-25 16:46:57 +0900

    Add PagesController

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+    def index; end
+
+    def billing; end
+
+    def dashboard; end
+
+    def icons; end
+
+    def map; end
+
+    def notifications; end
+
+    def profile; end
+
+    def rtl; end
+
+    def sign_in; end
+
+    def sign_up; end
+
+    def tables; end
+
+    def template; end
+
+    def typography; end
+
+    def virtual_reality; end
+end
